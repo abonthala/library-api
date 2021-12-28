0935ab4d16d99628df50a3520ed05b9076918014
Akshay Kumar Bonthala <abonthala@evoketechnologies.com>
2021-12-28 20:25:48 +0530

    Get Publisher API Commit

---
+++ src/main/java/com/skb/course/apis/libraryapis/exceptions/LibraryResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.skb.course.apis.libraryapis.exceptions;
+
+public class LibraryResourceNotFoundException extends RuntimeException {
+	
+	public LibraryResourceNotFoundException(String message)
+	{
+		super(message);
+	}
+
+}
