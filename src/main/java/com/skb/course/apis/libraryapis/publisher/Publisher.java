eb469ed524471b0e039df48b124662c90fa6dada
Akshay Kumar Bonthala <abonthala@evoketechnologies.com>
2021-12-29 17:22:45 +0530

    Added Logs to Controller

--- src/main/java/com/skb/course/apis/libraryapis/publisher/Publisher.java
+++ src/main/java/com/skb/course/apis/libraryapis/publisher/Publisher.java
@@ -1,10 +1,20 @@
 package com.skb.course.apis.libraryapis.publisher;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class Publisher {
 
     private Integer publisherId;
+    
+    @Size(min = 1, max = 50, message = "Publisher Name must be between 1 and 50 characters.")
     private String name;
+    
+    @Email(message = "Enter a valid emailId")
     private String emailId;
+    
+    @Pattern(regexp = "\\d{3}-\\d{3}-\\d{3}", message = "Please enter valid phone number.")
     private String phoneNumber;
 
     public Publisher() {
--- src/main/java/com/skb/course/apis/libraryapis/publisher/PublisherController.java
+++ src/main/java/com/skb/course/apis/libraryapis/publisher/PublisherController.java
@@ -2,6 +2,10 @@ package com.skb.course.apis.libraryapis.publisher;
 
 import java.util.UUID;
 
+import javax.validation.Valid;
+
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +30,8 @@ public class PublisherController {
 	
 	@Autowired
 	private PublisherService publisherService;
+	
+	private static org.slf4j.Logger logger = LoggerFactory.getLogger(PublisherController.class);
 
     @GetMapping(path = "/{publisherId}")
     public ResponseEntity<?> getPublisher(@PathVariable("publisherId") Integer publisherId,
@@ -39,13 +45,15 @@ public class PublisherController {
     		publisher = publisherService.getPublisher(publisherId, traceId);
     	}
     	catch(LibraryResourceNotFoundException e) {
+    		logger.error("getPublisher Controller {}", traceId);
     		return new ResponseEntity<>("Trace-Id "+traceId+" "+e.getMessage(), HttpStatus.NOT_FOUND);
     	}
+    	logger.info("getPublisher Controller {}", traceId);
         return new ResponseEntity<>(publisher, HttpStatus.OK);
     }
     
     @PostMapping(path = "/add")
-    public ResponseEntity<?> addPublisher(@RequestBody Publisher publisher, 
+    public ResponseEntity<?> addPublisher(@Valid @RequestBody Publisher publisher, 
     									  @RequestHeader(value="Trace-Id", defaultValue="") String traceId)
     {
     	if(!LibraryApiUtils.doesStringValueExists(traceId))
@@ -58,14 +66,16 @@ public class PublisherController {
     	}
     	catch(LibraryResourceAlreadyExistsException e)
     	{
+    		logger.error("addPublisher Controller {}", traceId);
     		return new ResponseEntity<>("Trace-Id "+traceId+" "+e.getMessage(), HttpStatus.CONFLICT);
     	}
+    	logger.info("addPublisher Controller {}", traceId);
 		return new ResponseEntity<>(publisher, HttpStatus.CREATED);
     }
     
     @PutMapping(path = "/{publisherId}")
-    public ResponseEntity<?> getPublisher(@PathVariable("publisherId") Integer publisherId,
-    									  @RequestBody Publisher publisher,
+    public ResponseEntity<?> updatePublisher(@PathVariable("publisherId") Integer publisherId,
+    									  @Valid @RequestBody Publisher publisher,
     									  @RequestHeader(value="Trace-Id", defaultValue="") String traceId) {
     	if(!LibraryApiUtils.doesStringValueExists(traceId))
     	{
@@ -76,8 +86,10 @@ public class PublisherController {
     		updatedPublisher = publisherService.updatePublisher(publisherId, publisher, traceId);
     	}
     	catch(LibraryResourceNotFoundException e) {
+    		logger.error("updatePublisher Controller {}", traceId);
     		return new ResponseEntity<>("Trace-Id "+traceId+" "+e.getMessage(), HttpStatus.NOT_FOUND);
     	}
+    	logger.info("updatePublisher Controller {}", traceId);
         return new ResponseEntity<>(updatedPublisher, HttpStatus.OK);
     }
     
@@ -93,8 +105,10 @@ public class PublisherController {
     		publisherService.deletePublisher(publisherId, traceId);
     	}
     	catch(LibraryResourceNotFoundException e) {
+    		logger.error("deletePublisher Controller {}", traceId);
     		return new ResponseEntity<>("Trace-Id "+traceId+" "+e.getMessage(), HttpStatus.NOT_FOUND);
     	}
+    	logger.info("deletePublisher Controller {}", traceId);
     	return new ResponseEntity<>("Publisher Successfully Deleted.", HttpStatus.OK);
     }
     
@@ -108,8 +122,10 @@ public class PublisherController {
     	}
     	if(!(LibraryApiUtils.doesStringValueExists(name)))
     	{
+    		logger.error("searchPublisher Controller {}", traceId);
     		return new ResponseEntity<>("Trace-Id "+traceId+" Please enter a name to search publisher.", HttpStatus.BAD_REQUEST);
     	}
+    	logger.info("searchPublisher Controller {}", traceId);
     	return new ResponseEntity<>(publisherService.searchPublisher(name, traceId), HttpStatus.OK);
     }
 }
